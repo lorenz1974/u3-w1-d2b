67f01d7ba9ce753b5ed3e1132a38b01043d738e0
Lorenz <l.lione@remoteitalia.com>
2024-11-26 16:56:07 +0100

    Ora funziona!

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -16,32 +16,34 @@ const booksArray = [
 ]
 
 function AllTheBooks(props) {
-  {
-    booksArray.map((book) => {
-      console.log(book.asin)
-      return (
-        <div key={book.asin} className='col mt-3'>
-          <Card>
-            <Card.Img
-              variant='top'
-              src={book.img}
-              alt={`Copertina di ${book.title}`}
-            />
-            <Card.Body>
-              <Card.Title>{book.title}</Card.Title>
-              <Card.Text>ASIN: {book.asin}</Card.Text>
-            </Card.Body>
-            <ListGroup className='list-group-flush'>
-              <ListGroup.Item>Prezzo: €{book.price}</ListGroup.Item>
-            </ListGroup>
-            <Card.Body>
-              <Card.Link href='#'>Compra</Card.Link>
-            </Card.Body>
-          </Card>
-        </div>
-      )
-    })
-  }
+  return (
+    <div className='row'>
+      {booksArray.map((book) => {
+        console.log(book.asin)
+        return (
+          <div key={book.asin} className='col mt-3'>
+            <Card>
+              <Card.Img
+                variant='top'
+                src={book.img}
+                alt={`Copertina di ${book.title}`}
+              />
+              <Card.Body>
+                <Card.Title>{book.title}</Card.Title>
+                <Card.Text>ASIN: {book.asin}</Card.Text>
+              </Card.Body>
+              <ListGroup className='list-group-flush'>
+                <ListGroup.Item>Prezzo: €{book.price}</ListGroup.Item>
+              </ListGroup>
+              <Card.Body>
+                <Card.Link href='#'>Compra</Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default AllTheBooks
